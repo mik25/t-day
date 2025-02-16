ce02e5a865d0d86d9302a0df7d3e193258232e4a
mik25 <mikmc55@gmail.com>
2025-02-16 13:37:58 +0000

    Update utils.js

--- utils.js
+++ utils.js
@@ -198,7 +198,7 @@ let fetchTorrent2 = async (query, type = "series") => {
   let hostdata = hosts[Math.floor(Math.random() * hosts.length)];
   if (!hostdata) return [];
 
-  let url = `${hostdata.host}/api/v2.0/indexers/all/results?apikey=${hostdata.apiKey}&Query=${query}&Tracker%5B%5D=iptorrents&Category%5B%5D=2000&Category%5B%5D=2040&Category%5B%5D=2045&Category%5B%5D=2050&Category%5B%5D=2080&Category%5B%5D=5000&Category%5B%5D=5030&Category%5B%5D=5040&Category%5B%5D=5080&Category%5B%5D=8000&Category%5B%5D=100020&Category%5B%5D=100022&Category%5B%5D=100026&Category%5B%5D=100048&Category%5B%5D=100065&Category%5B%5D=100068&Category%5B%5D=100073&Category%5B%5D=100076&Category%5B%5D=100099&Category%5B%5D=100100&Category%5B%5D=100101&cache=false`;
+  let url = `${hostdata.host}/api/v2.0/indexers/all/results?apikey=${hostdata.apiKey}&Query=${query}&Tracker%5B%5D=trellas=2000&category=5000&category=100001&category=100044&category=100052&category=100082&category=100084&category=100086&category=100087&category=100092&category=100099&cache=false`;
 
   return await fetch(url, {
     headers: {
