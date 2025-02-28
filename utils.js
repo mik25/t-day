9eeed8f6d774a32ef1032638ab97d68a53b57714
mik25 <mikmc55@gmail.com>
2025-02-28 06:11:36 +0000

    Update utils.js

--- utils.js
+++ utils.js
@@ -198,7 +198,7 @@ let fetchTorrent2 = async (query, type = "series") => {
   let hostdata = hosts[Math.floor(Math.random() * hosts.length)];
   if (!hostdata) return [];
 
-  let url = `${hostdata.host}/api/v2.0/indexers/all/results?apikey=${hostdata.apiKey}&Query=${query}&tracker=yourbittorrent&category=2000&category=5000&category=5000&cache=false`;
+  let url = `${hostdata.host}/api/v2.0/indexers/all/results?apikey=${hostdata.apiKey}&Query=${query}&tracker=yourbittorrent&category=2000&category=5000&category=8000&cache=false`;
 
   return await fetch(url, {
     headers: {
